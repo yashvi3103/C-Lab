f9b753a894cd0980fc6da92016425a2b3208ccfb
yashvi3103 <yashvi310305@gmail.com>
2023-08-31 18:09:27 +0530

    Coding till 31-08-2023

---
+++ script.js
@@ -0,0 +1,12 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('sidebar-collapsed');
+  }
+function togglenavbar() {
+    const navu = document.getElementById('navu');
+    navu.classList.toggle('snavu-collapsed');
+  }
+function rotate() {
+  const image  = document.querySelector('body');
+  image.classList.toggle('rotate')
+}
